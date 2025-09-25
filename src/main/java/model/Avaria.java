7737b2a5025a5b6b4cbed3827f130ee074f67dc7
DDiasss777 <danieldiasalmeida2009@gmail.com>
2025-09-25 13:33:50 -0300

    Sobrescrevendo conteúdo para igualar ao da falgadja_frontend

---
+++ src/main/java/model/Avaria.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Avarias {
+    private int id;
+    private String nome;
+    private String descricao;
+
+    // CONSTRUTOR
+
+    public Avarias(String nome, int id, String descricao) {
+        this.nome = nome;
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    // GETTERS
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    // SETTERS
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    // TO STRING
+
+    @Override
+    public String toString() {
+        return "Avarias{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/model/FuncionarioTurno.java
@@ -0,0 +1,7 @@
+package model;
+
+public class  FuncionarioTurno{
+    private int id;
+    private int idTurno;
+    private int idFuncionario;
+} 
